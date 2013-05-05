3326f39b96f6c2197ce9d36a85d4733d4ea0b8f5
CrasherCourse <micha454@umn.edu>
2013-05-04 22:06:58 -0500

    Took sample code to make server work

--- quote_server.c
+++ quote_server.c
@@ -13,7 +13,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define SERVER_PORT 6789 /* CHANGE THIS TO THE PORT OF YOUR SERVER */
+#define SERVER_PORT "6789" /* CHANGE THIS TO THE PORT OF YOUR SERVER */
 #define BUFFER_SIZE 1024
 #define QUOTE_NAME_SIZE 256
 #define QUOTE_FILE_TOTAL 10
@@ -114,12 +114,14 @@ void * clientThread(int * input)
 /**********************************************************************
  * main
  **********************************************************************/
-
+/*
 int main(int argc, char *argv[])
 {
 	int i, length, serverSocket;
 	struct sockaddr_in servaddr;
 	int * clientSocket;
+	struct sockaddr_storage clientaddr; // connector's address information
+    socklen_t sin_size;
 
 	printf("Start\n");
 	if(argc != 2)
@@ -145,11 +147,103 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 	printf("Bound Socket!\n");
+	if(getsockname(serverSocket, (struct sockaddr *)&servaddr, &length))
+	{
+		perror("geting socket's port number\n");
+		exit(0);
+	}
 	listen(serverSocket, BACKLOG);
 	printf("Ready to serve\n");
 	while(1)
 	{
-		break;
+
 	}
 	return 0;
 }
+* */
+void sigchld_handler(int s)
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+int main(void)
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, SERVER_PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        clientThread(&new_fd);
+    }
+
+    return 0;
+}
+
+
